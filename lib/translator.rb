1a305d528c56241d887ca3e6c5d47683d5643f7c
Menhajuddin Sharaf <menhajuddin.sharaf@gmail.com>
2021-01-13 23:01:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,2 +1,13 @@
 require "yaml"
 file = "emoticons.yml"
+
+def load_library(file)
+        readFile = YAML.load_file(file)
+        newHashFile = {}
+        readFile.each do |key, value|
+            # newHashFile = {key => {"English" => value[0], "Japanese" => value[1]}}
+            tempHash = {"english" => value[0], "japanese" => value[1]}
+            newHashFile.store(key, tempHash)
+        end
+        return newHashFile
+  end
