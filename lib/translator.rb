009616528c561aed99416a0c33f626a2c2cc19b3
Menhajuddin Sharaf <menhajuddin.sharaf@gmail.com>
2021-01-13 23:02:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file)
         newHashFile = {}
         readFile.each do |key, value|
             # newHashFile = {key => {"English" => value[0], "Japanese" => value[1]}}
-            tempHash = {"english" => value[0], "japanese" => value[1]}
+            tempHash = {:english => value[0], :japanese => value[1]}
             newHashFile.store(key, tempHash)
         end
         return newHashFile
